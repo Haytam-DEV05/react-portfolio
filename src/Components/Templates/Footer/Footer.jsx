3f4b542cd9914595560bb2b91e8fbb8b1a6a24eb
haitam <haitam@gmail.com>
2026-01-12 23:46:24 +0100

    switch the dynamique of the menu navigation

--- src/Components/Templates/Footer/Footer.jsx
+++ src/Components/Templates/Footer/Footer.jsx
@@ -2,42 +2,42 @@ import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { SiFrontendmentor } from "react-icons/si";
 
-export default function Footer() {
-  const socialIcons = [
-    {
-      id: 1,
-      icon: <FaGithub size={25} />,
-      href: "https://github.com/HAYTAM-DEV05",
-      aria_label: "Visit my GitHub profile",
-    },
-    {
-      id: 2,
-      icon: <FaLinkedin size={25} />,
-      href: "https://www.linkedin.com/in/haitam-full-stack-developer-nefal-649698361/",
-      aria_label: "Visit my LinkedIn profile",
-    },
-    {
-      id: 3,
-      icon: <SiFrontendmentor size={25} />,
-      href: "https://www.frontendmentor.io/profile/Haytam-DEV05",
-      aria_label: "Visit my Front-End profile",
-    },
-  ];
+export const socialIcons = [
+  {
+    id: 1,
+    icon: <FaGithub size={25} />,
+    href: "https://github.com/HAYTAM-DEV05",
+    aria_label: "Visit my GitHub profile",
+  },
+  {
+    id: 2,
+    icon: <FaLinkedin size={25} />,
+    href: "https://www.linkedin.com/in/haitam-full-stack-developer-nefal-649698361/",
+    aria_label: "Visit my LinkedIn profile",
+  },
+  {
+    id: 3,
+    icon: <SiFrontendmentor size={25} />,
+    href: "https://www.frontendmentor.io/profile/Haytam-DEV05",
+    aria_label: "Visit my Front-End profile",
+  },
+];
 
+export default function Footer() {
   return (
     <footer
       id="footer"
-      className="container py-5 text-center md:flex justify-between items-center border-t-2 border-[var(--soft)]"
+      className="container py-5 text-center md:flex justify-between items-center border-t-2 border-(--soft)"
     >
       <p>
         All Right Reserverd By{" "}
         <span className="primary cursor-pointer">Haitam Nefal</span>
       </p>
-      <ul className="flex gap-6 socials" >
+      <ul className="flex gap-6 socials">
         {socialIcons.map((ele) => (
           <li
             key={ele.id}
-            className="cursor-pointer max-w-fit mx-auto my-2 hover:text-[var(--primary)] transition-colors duration-200"
+            className="cursor-pointer max-w-fit mx-auto my-2 hover:text-(--primary) transition-colors duration-200"
           >
             <a href={ele.href} target="_blanc" aria-label={ele.aria_label}>
               {ele.icon}
--- src/Components/Templates/Header/Header.jsx
+++ src/Components/Templates/Header/Header.jsx
@@ -12,8 +12,9 @@ import About from "../../Pages/About/About";
 import Projects from "../../Pages/Projects/Projects";
 import Contact from "../../Pages/Contact/Contact";
 import Theme from "../Theme/Theme";
-
 import { HiMenu, HiX } from "react-icons/hi";
+
+import { socialIcons } from "../Footer/Footer";
 
 export default function Header() {
   const [rotatMenu, setRotateMenu] = useState(false);
@@ -49,8 +50,8 @@ export default function Header() {
   function Layout() {
     return (
       <>
-        <nav className="fixed w-full top-0 left-0 z-50 backdrop-blur-md border-b border-[var(--soft)] transition-colors duration-250 shadow-md">
-          <div className="container mx-auto px-6 md:px-12 flex justify-between items-center h-[70px]">
+        <nav className="fixed w-full top-0 left-0 z-50 backdrop-blur-md border-b border-(--soft) transition-colors duration-250 shadow-md">
+          <div className="container mx-auto px-6 md:px-12 flex justify-between items-center h-17.5">
             <div className="logo">
               <h2 className="text-[30px] font-semibold cursor-pointer">
                 <NavLink to="/">Haitam-NL</NavLink>
@@ -65,8 +66,8 @@ export default function Header() {
                     to={link.path}
                     className={({ isActive }) =>
                       isActive
-                        ? "text-[var(--primary)] border-b-2 border-[var(--primary)] pb-1"
-                        : "hover:text-[var(--primary)] transition"
+                        ? "text-(--primary) border-b-2 border-(--primary) pb-1"
+                        : "hover:text-(--primary) transition"
                     }
                   >
                     {link.nameLinke}
@@ -96,27 +97,57 @@ export default function Header() {
 
             {/* MOBILE UL */}
             <ul
-              className={`md:hidden flex flex-col ps-4 gap-4 py-6 fixed top-[70px] left-0 right-0  bg-[var(--bg)]/80`}
-              style={{ height: open ? "auto" : "0", opacity: open ? 1 : 0 }}
+              className={`md:hidden fixed top-0 -right-25 h-screen bg-(--bg) py-10 px-6 transition-all duration-1000 shadow-2xl ${
+                open ? "showMenu" : "closeMenu"
+              }`}
             >
+              <li
+                className="mb-6 cursor-pointer rotate max-w-fit"
+                onClick={() => {
+                  setOpen(false);
+                  setRotateMenu(!rotatMenu);
+                }}
+              >
+                <HiX size={25} />
+              </li>
+
               {navLinkes.map((link) => (
-                <li key={link.id} onClick={() => setOpen(false)}>
+                <li key={link.id} className="mb-4">
                   <NavLink
                     to={link.path}
+                    onClick={() => setOpen(false)}
                     className={({ isActive }) =>
                       isActive
-                        ? "text-[var(--primary)] font-semibold border-b-2"
-                        : "hover:text-[var(--primary)]"
+                        ? "text-(--primary) font-semibold border-b-2 border-(--primary) text-[20px]"
+                        : "hover:text-(--primary) text-[20px]"
                     }
                   >
                     {link.nameLinke}
                   </NavLink>
                 </li>
               ))}
+              <div className=""></div>
+              <div className="bottom-nav-menu w-full pt-5">
+                <div className="social-icons absolute bottom-5 flex justify-around w-[90%]">
+                  {socialIcons.map((social) => {
+                    return (
+                      <a
+                        href={social.href}
+                        key={social.id}
+                        aria-label={social.aria_label}
+                        className="cursor-pointer hover:text-(--primary) hover:-translate-y-2 transition-all duration-200"
+                        target="_blank"
+                      >
+                        {social.icon}
+                      </a>
+                    );
+                  })}
+                </div>
+              </div>
             </ul>
           </div>
         </nav>
-        <main className="px-3 min-h-[90vh] pt-[100px] flex justify-center items-center flex-col">
+        <main className="px-3 min-h-[90vh] pt-25 flex justify-center items-center flex-col">
           <Outlet />
         </main>
       </>
