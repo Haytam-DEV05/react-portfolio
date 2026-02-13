13b35d2b8194c23c4d7c78ccf2ba404f8ae1cf22
Haytam-DEV05 <nefalhaytam57@gmail.com>
2026-02-13 17:44:15 +0100

    make a single source of data

--- src/Data/DataProjects.js
+++ src/Data/DataProjects.js
@@ -1,4 +1,3 @@
-
 export const DataProjects = [
   {
     id: 1,
@@ -6,8 +5,14 @@ export const DataProjects = [
     img_path: '/images/front-end-Ecomme.png',
     github_path: 'https://github.com/Haytam-DEV05/mini-ecommerce',
     live_path: 'https://mini-ecommerce-one-delta.vercel.app/',
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-              modi eum exercitationem dolorem consequuntur possimus.`
+    description: `This is a front-end e-commerce web application built using HTML, CSS, and JavaScript.
+                It simulates a real shopping experience where users can register, log in, add products to their cart, and complete orders.
+                An admin dashboard is also included to manage users and check orders — all handled using LocalStorage.`,
+    tech_stack: [
+      {
+        front_end: ['HTML', 'CSS3', 'Javascript (ES6)', 'SweetAlert2']
+      }
+    ]
   },
   {
     id: 2,
@@ -16,7 +21,14 @@ export const DataProjects = [
     github_path: 'https://github.com/Haytam-DEV05/fullstack-ecommerce-app',
     live_path: false,
     description: `Full-stack e-commerce web application built with HTML, CSS, JavaScript, PHP, and MySQL. Features product management, 
-                    shopping cart, checkout system, and real-time stock updates.`
+                    shopping cart, checkout system, and real-time stock updates.`,
+    tech_stack: [
+      {
+        front_end: ['HTML5', 'CSS3', 'Javascript'],
+        back_end: ['PHP'],
+        database: ['Mysql']
+      }
+    ]
   },
   {
     id: 3,
@@ -37,23 +49,5 @@ export const DataProjects = [
     description: `A responsive landing page built using only HTML and CSS. The project includes 
                     multiple sections: Navigation, Articles, Gallery, Features, Testimonials, Team, Services, Skills, 
                     Pricing, Videos, and Stats. The goal of this project is to practice building professional interfaces using Flexbox, Grid, and Animations.`
-  },
-  {
-    id: 5,
-    title: 'Lorem, ipsum dolor.',
-    img_path: '/images/js.webp',
-    github_path: '',
-    live_path: false,
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-              modi eum exercitationem dolorem consequuntur possimus.`
-  },
-  {
-    id: 6,
-    title: 'Lorem, ipsum dolor.',
-    img_path: '/images/tailwind.webp',
-    github_path: '',
-    live_path: false,
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-              modi eum exercitationem dolorem consequuntur possimus.`
   }
 ]
