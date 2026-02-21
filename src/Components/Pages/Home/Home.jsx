9c2829d5eb383c69180e79ed35e164a37179b3bc
Haytam-DEV05 <nefalhaytam57@gmail.com>
2026-02-21 17:49:20 +0000

    make the folder of cv to download it

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -9,10 +9,8 @@ export default function Home() {
           <h1 className="text-[55px] font-bold">
             Hi, I'm <span className="primary">Haitam Nefal</span>
           </h1>
-          <h2 className="text-[28px] md:my-[2rem]">
-            Full-Stack / Web Developer
-          </h2>
-          <p className="md:max-w-[500px]">
+          <h2 className="text-[28px] md:my-8">Full-Stack / Web Developer</h2>
+          <p className="md:max-w-125">
             I am a second-year Digital Development student at OFPPT, passionate
             about web development. I work with HTML, CSS, JavaScript, PHP,
             React, and Laravel to build modern, responsive, and user-friendly
@@ -20,14 +18,17 @@ export default function Home() {
             and real-world projects.
           </p>
           <div className="flex flex-col buttons my-3 md:flex-row gap-x-10 gap-y-3">
-            <button className="bg-[var(--primary)] px-2 py-1 cursor-pointer rounded-2xl text-[#fff]" aria-label="downoald-cv">
-              <a href="/src/assets/cv/bootstrap.jpg" download>
-                Download Cv
+              <a
+                className="bg-(--primary) px-2 py-1 cursor-pointer rounded-2xl text-white text-center"
+                href="/cv/Haitam_Nefal_CV.pdf"
+                aria-label="downoald-cv"
+                download
+              >
+                Download CV
               </a>
-            </button>
             <NavLink
-            aria-label="open-contact-form"
-              className="bg-[var(--primary)] px-5 py-1.5 cursor-pointer rounded-2xl text-center text-[#fff]"
+              aria-label="open-contact-form"
+              className="bg-(--primary) px-5 py-1.5 cursor-pointer rounded-2xl text-center text-white"
               to="/Contact"
             >
               Contact Me
